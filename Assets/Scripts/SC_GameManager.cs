73c7b404ab354cff7a98a403cd94c8b8cd6e139d
Diego A. Molina <diegomolina.a.c@gmail.com>
2023-09-12 15:56:59 -0300

    before camera problems

--- Assets/Scripts/SC_GameManager.cs
+++ Assets/Scripts/SC_GameManager.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class SC_GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject circle;
+    [SerializeField] GameObject ex;
+    //[SerializeField] Transform parentTransform;
 
-    // Update is called once per frame
-    void Update()
+    void Update () 
     {
-        
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                Instantiate(circle, touchPos, Quaternion.identity);
+                
+            }
+        }
     }
 }
